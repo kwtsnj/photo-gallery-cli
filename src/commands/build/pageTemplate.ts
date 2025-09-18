7f09c8dc71b55df37c9ec524f9eee07341110f93
kwtsnj <225404664+kwtsnj@users.noreply.github.com>
2025-09-19 08:52:50 +0900

    feat: 背景をつけてみる

--- src/commands/build/pageTemplate.ts
+++ src/commands/build/pageTemplate.ts
@@ -11,6 +11,21 @@ export const pageTemplate = (gallery: string) => html`
       <style>
         body {
           margin: 0;
+          background-image:
+            linear-gradient(#444cf7 1px, transparent 1px),
+            linear-gradient(to right, #444cf7 1px, #e5e5f7 1px);
+          background-size: 20px 20px;
+        }
+
+        body::before {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          background-color: #e5e5f7;
+          opacity: 0.8;
+          z-index: -1;
         }
 
         .container {
@@ -45,12 +60,12 @@ export const pageTemplate = (gallery: string) => html`
         }
       </style>
     </head>
-    <body class="">
+    <body>
       <!-- モーダル -->
       <div id="overlay" class="overlay">
         <img src="" alt="" />
       </div>
-      <div class="gallery">${gallery}</div>
+      <div>${gallery}</div>
       ${lazyImageScript}
       <script>
         const overlay = document.getElementById('overlay');
