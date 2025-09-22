2cf0b815450bc9d70f068a74eb2b0a3dd6353f2d
kwtsnj <225404664+kwtsnj@users.noreply.github.com>
2025-09-22 17:06:33 +0900

    feat: フォルダ画像をli内に追加し、フォルダアイコン押下でのリンクに対応

--- src/commands/build/buildCommandHandler.ts
+++ src/commands/build/buildCommandHandler.ts
@@ -70,7 +70,7 @@ export class GalleryHandler implements BuildCommandHandler {
           ${photoElements.join('\n')}
         </image-container>`);
       const directoryName = path.basename(breadcrumbs) || '/';
-      const anchor = `<a href="#${address}">${directoryName} (${imageCount})</a>`;
+      const anchor = `<a href="#${address}">${this.buildFolderSVG()} ${directoryName} (${imageCount})</a>`;
       sidebarElement = `<li>${anchor}</li>`;
     }
 
@@ -97,8 +97,8 @@ export class GalleryHandler implements BuildCommandHandler {
       if (sidebarListElements.length > 0 || fileExists) {
         const directoryName = path.basename(breadcrumbs) || '/';
         const anchorOrSpan = fileExists
-          ? `<a href="#${address}">${directoryName} (${imageCount})</a>`
-          : `<span>${directoryName}</span>`;
+          ? `<a href="#${address}">${this.buildFolderSVG()} ${directoryName} (${imageCount})</a>`
+          : `<span>${this.buildFolderSVG()} ${directoryName}</span>`;
         const childrenHtml =
           sidebarListElements.length > 0
             ? `\n<ul>${sidebarListElements.join('\n')}</ul>`
@@ -132,5 +132,16 @@ export class GalleryHandler implements BuildCommandHandler {
 
   private isFile(node: ContentsNode): node is ContentsNodeWithType<'file'> {
     return node.type === 'file';
+  }
+
+  private buildFolderSVG(): string {
+    return `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <g transform="translate(1.4 1.4) scale(2.81 2.81)">
+          <path d="M0 68.798v11.914c0 1.713 1.401 3.114 3.114 3.114h0c3.344 0 4.805-2.642 4.805-2.642L8.14 29.281l2.739-2.827l72.894-2.977v-1.482c0-2.396-1.942-4.338-4.338-4.338H50.236c-1.15 0-2.254-0.457-3.067-1.27l-8.943-8.943c-0.813-0.813-1.917-1.27-3.067-1.27H4.338C1.942 6.174 0 8.116 0 10.512v7.146v2.332V68.798" fill="rgb(224,173,49)"/>
+          <path d="M3.114 83.826c1.713 0 3.114-1.401 3.114-3.114V27.81c0-2.393 1.94-4.333 4.333-4.333h75.107c2.393 0 4.333 1.94 4.333 4.333v51.684c0 2.393-1.94 4.333-4.333 4.333H3.114z" fill="rgb(255,200,67)"/>
+        </g>
+      </svg>
+    `;
   }
 }
